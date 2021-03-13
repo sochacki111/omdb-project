4d0eb397bc066184e76fb508f5db85db2cac8541
sochacki111 <michalsochacki111@gmail.com>
2021-03-13 18:56:26 +0100

    add search movie form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import axios from './axios-base';
+import { Input, FormControl, Button } from '@material-ui/core';
 import './App.css';
+const { useState, useEffect, useCallback } = React;
 
 function App() {
+  const [text, setText] = useState('');
+
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { data } = await axios.get(`movies/search-by-title?t=${text}`);
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      Hello world!
+    <div>
+      <form onSubmit={submitHandler}>
+        <FormControl>
+          <Input
+            type="text"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setText(e.target.value)
+            }
+          />
+        </FormControl>
+        <Button type="submit" value="Submit">
+          Search
+        </Button>
+      </form>
     </div>
   );
 }
