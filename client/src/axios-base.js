a74cd36dbe8099f3e477792e061c146f91b97635
sochacki111 <michalsochacki111@gmail.com>
2021-03-14 23:10:11 +0100

    refine

--- client/src/axios-base.js
+++ client/src/axios-base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_DOMAIN || 'http://localhost:8080/'
+  baseURL: process.env.REACT_APP_BACKEND_DOMAIN
 });
 
 export default instance;
